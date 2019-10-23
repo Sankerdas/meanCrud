7c7749603101cd222aed507f02ee007b7af85e46
Sanker das <sankerdas.yes@gmail.com>
2019-10-23 22:40:06 +0530

    router and nav bar added
    router and nav bar created and added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { GstAddComponent } from './gst-add/gst-add.component';
+import { GstGetComponent } from './gst-get/gst-get.component';
+import { GstEditComponent } from './gst-edit/gst-edit.component';
+
+const routes: Routes = [
+  {
+    path: 'business/create',
+    component: GstAddComponent
+  },
+  {
+    path: 'business/edit/:id',
+    component: GstEditComponent
+  },
+  {
+    path: 'business',
+    component: GstGetComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
