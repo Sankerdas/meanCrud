d217c4202becbc832be1bff5cf914b4e9cb33a01
Sanker das <sankerdas.yes@gmail.com>
2019-10-29 18:03:07 +0530

    Backend created
    backend created (node express mongo mongoose)
    
    a foldred namded as backend and nesseray files added
    
    npm init -y
    npm install --save express body-parser cors mongoose
    npm install nodemon --save-dev
    
    server.js file contains backed code
    
    to start server -
    nodemon server

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express'),
+      path = require('path'),
+      bodyParser = require('body-parser'),
+      cors = require('cors'),
+      mongoose = require('mongoose');
+
+      const app = express();
+      let port = process.env.PORT || 4000;
+
+      server = app.listen(function(){
+          console.log('listening on port '+port);
+      });
+      
